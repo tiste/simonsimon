cb52d30fb1c8072a04b40ce9526667a8f41037fb
Baptiste Lecocq <hello@tiste.io>
2015-04-16 16:49:07 +0200

    Add style, add main layout

--- app/scripts/simonsimon.js
+++ app/scripts/simonsimon.js
@@ -1,1 +1,38 @@
 'use strict';
+
+var Simon = function() {},
+    app   = new Simon(),
+    keys  = {
+      49: '1',
+      50: '2',
+      51: '3',
+      52: '4',
+      53: '5',
+      54: '6',
+      55: '7',
+      56: '8',
+      57: '9'
+    };
+
+Simon.prototype.press = function(nb) {
+  $('.tile[data-tile="' + nb + '"]').addClass('pressed');
+
+  setTimeout(function() {
+    $('.tile[data-tile="' + nb + '"]').removeClass('pressed');
+  }, 200);
+};
+
+Simon.prototype.getNbFromKey = function(key) {
+  return keys[key];
+};
+
+$(document).on('keypress', function(e) {
+  var nb = app.getNbFromKey(e.keyCode ? e.keyCode : e.charCode);
+
+  if (nb)
+    app.press(nb);
+});
+
+$('.tile').on('click', function() {
+  app.press($(this).data('tile'));
+});
